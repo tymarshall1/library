f46e7a5eba720fd4265f5a0d03468685b662bbc6
Tyler Marshall <60017345+tymarshall1@users.noreply.github.com>
2023-09-21 15:44:10 -0400

    created a function to make a card for a book

--- script.js
+++ script.js
@@ -22,7 +22,59 @@ myLibrary.push(harryPotter);
 myLibrary.push(theShining);
 myLibrary.push(theOutsider);
 
-console.log(myLibrary);
+function makeCard() {
+  const cardContainer = document.querySelector(".card-container");
+
+  const card = document.createElement("div");
+  card.classList.add("card");
+
+  const title = document.createElement("div");
+  const author = document.createElement("div");
+  const pages = document.createElement("div");
+  const hasRead = document.createElement("div");
+
+  const bookTitleHeader = document.createElement("h3");
+  bookTitleHeader.textContent = "Title";
+  const bookTitlePara = document.createElement("p");
+  bookTitlePara.textContent = "placeholder";
+
+  const bookAuthorHeader = document.createElement("h3");
+  bookAuthorHeader.textContent = "Author";
+  const bookAuthorPara = document.createElement("p");
+  bookAuthorPara.textContent = "placeholder";
+
+  const bookPageNumHeader = document.createElement("h3");
+  bookPageNumHeader.textContent = "Pages";
+  const bookPageNumPara = document.createElement("p");
+  bookPageNumPara.textContent = "placeholder";
+
+  const bookHasReadHeader = document.createElement("h3");
+  bookHasReadHeader.textContent = "Read";
+  const bookHasReadinput = document.createElement("input");
+  bookHasReadinput.type = "checkbox";
+
+  title.appendChild(bookTitleHeader);
+  title.appendChild(bookTitlePara);
+  author.appendChild(bookAuthorHeader);
+  author.appendChild(bookAuthorPara);
+  pages.appendChild(bookPageNumHeader);
+  pages.appendChild(bookPageNumPara);
+  hasRead.appendChild(bookHasReadHeader);
+  hasRead.appendChild(bookHasReadinput);
+
+  card.appendChild(title);
+  card.appendChild(author);
+  card.appendChild(pages);
+  card.appendChild(hasRead);
+
+  cardContainer.appendChild(card);
+}
+
+function drawlCards() {
+  makeCard();
+}
+
+drawlCards();
 
 function addBookToLibrary() {
   // do stuff here
